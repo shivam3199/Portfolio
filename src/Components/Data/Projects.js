08d82e9d88b33b9291ab2f54df1bc629fb1a6723
Shivam Raskonda <shivamraskonda589@gmail.com>
2023-06-20 14:22:41 +0530

    Some FIxes

--- src/Components/Data/Projects.js
+++ src/Components/Data/Projects.js
@@ -1,7 +1,7 @@
 export const ProjectData = [
     {
         id: 1,
-        name: "Project 1",
+        name: "Motivation app",
         about: "The Random Quote Generator is an application that generates random quotes and allows users to share their favorite quotes on Twitter. Additionally, users have the option to save the quotes they like in PDF format. This app provides an engaging and interactive way for users to discover inspirational and thought-provoking quotes, while also giving them the ability to easily share and save their favorite quotes for future reference.",
         tags: ["Reactjs", "JavaScript", "Apis", "Hooks"],
         demo: 'https://motivationshivam.netlify.app/',
@@ -9,15 +9,15 @@ export const ProjectData = [
         image: require('../../Assets/icons/motivations.png'),
 
     },
-    // {
-    //     id: 2,
-    //     name: "Project 2",
-    //     about: "This is my 2nd project",
-    //     tags: ["MERN", "HTML", "CSS"],
-    //     demo: 'amnlandnaldn',
-    //     github: 'nalnlan',
-    //     image: require('../../Assets/icons/project2.svg').default,
+    {
+        id: 2,
+        name: "SWEAT BUDDY",
+        about: "SWEAT BUDDY is a workout tracking application that allows users to conveniently monitor their fitness routines. Please note that this app has not been deployed yet. Once deployed, users can keep track of their workouts, add new exercises on a daily basis, and remove previous workouts as needed. One of the key features of SWEAT BUDDY is its ability to display the timestamp for each workout, allowing users to see when a specific workout was added. This timestamp feature provides users with a clear timeline of their fitness progress. Whether users are tracking their gym sessions, outdoor activities, or home workouts, SWEAT BUDDY helps them stay organized and motivated on their fitness journey.",
+        tags: ["MongoDb", "Express JS", "React JS", "Node js", "HTML", "CSS", "JavaScript","Material Ui"],
+        demo: 'amnlandnaldn',
+        github: 'https://github.com/shivam3199/SWEAT-BUDDY-',
+        image: require('../../Assets/icons/sweat-buddy.png'),
 
-    // },
+    },
 
 ]
--- src/Components/Data/work.js
+++ src/Components/Data/work.js
@@ -2,10 +2,10 @@ export const WorkData = [
     {
         company: "Persistent Systems",
         designation: "Software Engineer",
-        dateJoining: "13-sep-2021",
+        dateJoining: "june-2021",
         dateEnd: "Present",
         companyLogo: "https://th.bing.com/th/id/R.9d24806d4cd5315c5f81e062e69ed781?rik=2Phihpj8xPgmRw&riu=http%3a%2f%2fwww.icica.co.in%2fImages%2fPersistent_Logo.png&ehk=jLurNtpYHy6BMkHtT5kZkwoTUrxUvkQTvRjhfB6g1T4%3d&risl=&pid=ImgRaw&r=0",
-        work: "Having good knowledge of Reactjs, Javascript, Git and Carbon design systems.Highly adaptive to work on any type of work environment and technology"
+        work: "Having good knowledge of Reactjs, Javascript, Git, Carbon design systems and also some of he backend technologies such as Node Js and Express Js. Highly adaptive to work on any type of work environment and technology"
     },
 
 ];
