ff09661ffc374190feab386885ecf451d1d9ad6c
Shivam Raskonda <shivamraskonda589@gmail.com>
2023-07-13 21:10:10 +0530

    Added TO DO APP

--- src/Components/Data/Projects.js
+++ src/Components/Data/Projects.js
@@ -11,6 +11,16 @@ export const ProjectData = [
     },
     {
         id: 2,
+        name: "TO DO APP",
+        about: "Imagine a world where managing your tasks and staying organized becomes effortless, empowering you to accomplish more than you ever thought possible. Say hello to our awe-inspiring To-Do app, your ultimate companion in conquering the chaos of everyday life.With our cutting-edge features and user-friendly interface, our app is designed to seamlessly integrate into your busy schedule, helping you prioritize and tackle your to-dos with finesse. Here's what makes our app truly awe-inspiring:",
+        tags: ["JavaScript", "React JS", "HTML", "CSS", "React-icons"],
+        demo: 'https://shivamtodoo.netlify.app/',
+        github: 'https://github.com/shivam3199/To-Do',
+        image: require('../../Assets/icons/TODO.png'),
+
+    },
+    {
+        id: 3,
         name: "SWEAT BUDDY",
         about: "SWEAT BUDDY is a workout tracking application that allows users to conveniently monitor their fitness routines. Please note that this app has not been deployed yet. Once deployed, users can keep track of their workouts, add new exercises on a daily basis, and remove previous workouts as needed. One of the key features of SWEAT BUDDY is its ability to display the timestamp for each workout, allowing users to see when a specific workout was added. This timestamp feature provides users with a clear timeline of their fitness progress. Whether users are tracking their gym sessions, outdoor activities, or home workouts, SWEAT BUDDY helps them stay organized and motivated on their fitness journey.",
         tags: ["MongoDb", "Express JS", "React JS", "Node js", "HTML", "CSS", "JavaScript","Material Ui"],
